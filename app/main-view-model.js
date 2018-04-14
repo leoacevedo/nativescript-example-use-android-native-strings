e8b9ab27caac16ec564d1c757286be4e944a81de
Leonardo Acevedo <leo.acevedo@gmail.com>
2018-04-14 20:37:20 -0300

    Show native string at the top of the UI

--- app/main-view-model.js
+++ app/main-view-model.js
@@ -1,5 +1,27 @@
 var Observable = require("data/observable").Observable;
 
+// This module will give us access to the native Android context
+// See https://docs.nativescript.org/core-concepts/utils
+var utilsModule = require("tns-core-modules/utils/utils");
+
+// This method receives a native Android string ID name (like "example_string"),
+// and it returns a native Android integer ID
+const getAndroidStringId = utilsModule.ad.resources.getStringId;
+
+// Android Application object.
+// This object's getString() method receives a native Android integer ID,
+// and returns an actual Android native string
+const androidApp = utilsModule.ad.getApplication()
+
+/*
+ *  @param id: a string with the name of the native ID
+ *  @return  : the native Android string with that ID
+ */
+function getAndroidNativeString(idName) {
+    var androidId = getAndroidStringId(idName);
+    return androidApp.getString(androidId);
+}
+
 function getMessage(counter) {
     if (counter <= 0) {
         return "Hoorraaay! You unlocked the NativeScript clicker achievement!";
@@ -12,6 +34,7 @@ function createViewModel() {
     var viewModel = new Observable();
     viewModel.counter = 42;
     viewModel.message = getMessage(viewModel.counter);
+    viewModel.androidString = getAndroidNativeString('example_string')
 
     viewModel.onTap = function() {
         this.counter--;
